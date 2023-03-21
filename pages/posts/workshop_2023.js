bc9f2bae165f1e5fcb718700b51d86807bba04d5
Brandon Lincoln Snyder <86706338+branlsnyder@users.noreply.github.com>
2023-03-21 16:42:09 -0400

    Update workshop_2023.js
    added workshop dates

--- pages/posts/workshop_2023.js
+++ pages/posts/workshop_2023.js
@@ -7,26 +7,34 @@ export default function FirstPost() {
       <Head>
         <title>Web Audio Workshop</title>
       </Head>
-      
+
       <p className="event">
-      <h1>Web Audio for Non-Programmers</h1>
-      Details for our Workshop will be released soon. However, for now, please see this short description below.
+        <h1>Web Audio for Non-Programmers</h1>
+        Details for our Workshop will be released soon. However, for now, please see this short description below.
 
       </p>
 
       <p className="event">
         <h2>Workshop</h2>
-        <h4>6 Meetings in June/July - Specific Times TBA</h4>
+        <h4>6 Meetings in June/July - Tuesdays 17:00-19:00 Germany Time / 11:00-1:00 EST</h4>
         <h4>Online</h4>
-        <p>
-        Over the course of six 90-minute zoom meetings, workshop participants will learn how to program audio in the web browser using the p5.js library. 
+        <p className="event ul">
+          Tuesday, June 6th
+          Tuesday, June 13th
+          Tuesday, June 20th
+          Tuesday, June 27th
+          Tuesday, July 4th
+          Tuesday, July 11th
         </p>
-     
-        <figure> 
-        <img src="/images/ZoomRoom-1.png" classname="image-test"></img>
-         <figcaption>Laurens Inauen's Workshop from Browser Sound 2021</figcaption>
-         </figure>
-      
+        <p>
+          Over the course of six zoom meetings, workshop participants will learn how to program audio in the web browser using the p5.js library.
+        </p>
+
+        <figure>
+          <img src="/images/ZoomRoom-1.png" classname="image-test"></img>
+          <figcaption>Laurens Inauen's Workshop from Browser Sound 2021</figcaption>
+        </figure>
+
       </p>
 
 
