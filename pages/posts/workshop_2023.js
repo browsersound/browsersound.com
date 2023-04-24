0309a67ea088c81af77aa2abdcb391d9233a35a3
Brandon Lincoln Snyder <86706338+branlsnyder@users.noreply.github.com>
2023-04-23 22:16:43 -0400

    Update workshop_2023.js

--- pages/posts/workshop_2023.js
+++ pages/posts/workshop_2023.js
@@ -10,13 +10,20 @@ export default function FirstPost() {
 
       <p className="event">
         <h1>Javascript for Sound Artists</h1>
-        Register <a href="https://docs.google.com/forms/d/e/1FAIpQLSeUKm7gud1Dj81DplIT8Cz-qpRWgIFbf3IF97cDQgc98PNRKQ/viewform?usp=sf_link">Here</a>
-
-
+        <h2>Register <a href="https://docs.google.com/forms/d/e/1FAIpQLSeUKm7gud1Dj81DplIT8Cz-qpRWgIFbf3IF97cDQgc98PNRKQ/viewform?usp=sf_link">Here</a>
+        </h2>
       </p>
 
       <p className="event">
         <h2>Workshop</h2>
+        <p>These workshops are aimed for audiences with beginner/no-prior understanding of web programming. A major learning objective of these workshops is to equip you with fundamental tools and understanding of web programming, to kickstart a larger creative journey with the web.
+        </p>
+        <p> There is a registration fee for participation:</p>
+        <p>Regular Fee - €22.50 Euro  / $25.00 USD (regular)</p>
+        <p>Reduced Fee - €13.50 Euro / $5.25 (reduced)</p>
+        <p>
+          Reduced Fee is available, no questions asked, for students and anyone else for whom the regular fee is prohibitive.
+        </p>
         <h4>6 Meetings</h4>
         <h4>Online - All Meetings at 17:00-19:00 CET / 11:00-1:00 EST</h4>
         <p className="event ul">Tuesday, June 6th</p>
@@ -28,14 +35,7 @@ export default function FirstPost() {
         <p>This is an intro to Javascript crash course designed for musicians and sound artists. Through 6 online meetings, participants are taught the basics of javascript through the lens of the p5.js library for creative coding. We will touch upon handling audio files and synthesizing sound in the web browser, creating an interactive web page, producing audio-synced visuals, and other topics that will set up a musician to begin creating sound art in the web browser. The workshops will culminate with a reading session at Location Sharing, Browser Sound’s 2023 festival. There, participants will present a final project, and have the option to have Browser Sound’s ensemble read through a performance of their work.
         </p>
 
-        <p>These workshops are aimed for audiences with beginner/no-prior understanding of web programming. A major learning objective of these workshops is to equip you with fundamental tools and understanding of web programming, to kickstart a larger creative journey with the web.
 
-          There is a registration fee for participation:
-          Regular Fee - €22.50 Euro  / $25.00 USD (regular)
-          Reduced Fee - €13.50 Euro / $5.25 (reduced)
-
-          Reduced Fee is available, no questions asked, for students and anyone else for whom the regular fee is prohibitive.
-        </p>
 
         <figure>
           <img src="/images/ZoomRoom-1.png" classname="image-test"></img>
@@ -70,6 +70,6 @@ export default function FirstPost() {
 
 
 
-    </Layout>
+    </Layout >
   );
 }
